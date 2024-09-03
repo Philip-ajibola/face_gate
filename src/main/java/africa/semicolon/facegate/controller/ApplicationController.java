ff138cd860b1dfa3e7845ab37c572486d2fce3cd
philip <ajibolaphilip10@gmail.com>
2024-09-03 12:16:27 +0100

    commit

--- src/main/java/africa/semicolon/facegate/controller/ApplicationController.java
+++ src/main/java/africa/semicolon/facegate/controller/ApplicationController.java
@@ -1,12 +1,13 @@
 package africa.semicolon.facegate.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/")
 public class ApplicationController {
-    @RequestMapping(value = "/")
+    @PostMapping(value = "/")
     public String hello() {
         return "Hello Joshua nice trick!  ";
     }
