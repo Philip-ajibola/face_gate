1414a3905ae1c54ba0e0d8d6f420ba939b1b5c91
philip <ajibolaphilip10@gmail.com>
2024-09-03 10:05:37 +0100

    initail commit

---
+++ src/main/java/africa/semicolon/facegate/FaceGateApplication.java
@@ -0,0 +1,13 @@
+package africa.semicolon.facegate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaceGateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FaceGateApplication.class, args);
+    }
+
+}
---
+++ src/main/java/africa/semicolon/facegate/controller/ApplicationController.java
@@ -0,0 +1,13 @@
+package africa.semicolon.facegate.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class ApplicationController {
+    @RequestMapping(value = "/")
+    public String hello() {
+        return "Hello Joshua!";
+    }
+}
